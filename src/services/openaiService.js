17a25c01b8a44bc4eb6711596713332da0e301c4
Suhail Ansari <suhafea@gmail.com>
2024-03-06 21:14:22 +0530

    changing openai logic

--- src/services/openaiService.js
+++ src/services/openaiService.js
@@ -1,25 +1,35 @@
-const { OpenAI } = require('openai');
-const dotenv = require('dotenv')
+const axios = require('axios');
+const dotenv = require('dotenv');
 dotenv.config();
 
-const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const OPENAI_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
 
-async function generateResponse(partial_text) {
+async function generateResponse(partialText) {
   try {
-    const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: partial_text,
+    const response = await axios.post(
+      OPENAI_ENDPOINT,
+      {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'user',
+            content: partialText,
+          },
+        ],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${OPENAI_API_KEY}`,
         },
-      ],
-    });
+      }
+    );
 
-    return completion.data;
+    return response.data;
   } catch (error) {
     console.error('Error:', error);
-    throw error; 
+    throw error;
   }
 }
 
